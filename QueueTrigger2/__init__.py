8861c61b2698091ebce0eae67d37e58b4c7ac7dd
rrajaram7991 <rrajaram7991@gmail.com>
2023-06-30 10:26:36 +0530

    new code

---
+++ QueueTrigger2/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+import azure.functions as func
+
+
+def main(msg: func.QueueMessage) -> None:
+    logging.info('Python queue trigger function processed a queue item: %s',
+                 msg.get_body().decode('utf-8'))
+    #testing
+    #cli
+    #using devops
+    
